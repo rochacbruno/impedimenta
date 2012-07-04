64338fdb53e62131533335e071cd74dad8345f22
Jeremy Audet <ichimonji10@gmail.com>
2012-07-04 05:47:53 -0400

    Partially done adding new installable module.

---
+++ python/setup1/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""Install 'main' as module MODULE_NAME."""
+
+from distutils.core import setup
+
+setup(
+    name = 'foo',             # package metadata
+    version = '1.0',          # package metadata
+    py_modules = ['main'],    # package contents
+)
