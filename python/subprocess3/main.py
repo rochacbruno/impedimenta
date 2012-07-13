5c2487e0fe35a8d4402195e7dc231430d5900d09
Jeremy Audet <ichimonji10@gmail.com>
2012-07-12 21:30:48 -0400

    Update sample python code.

--- python/subprocess3/main.py
+++ python/subprocess3/main.py
@@ -3,15 +3,46 @@
 
 import subprocess
 
-INTERFACE_NAME = 'spam'
+def iface_up(iface_name):
+    """Enable a network interface"""
 
-print('Bringing up interface {}'.format(INTERFACE_NAME))
-try:
-    subprocess.check_output(
-        ['ifconfig', INTERFACE_NAME, 'up'],
-        stderr = subprocess.STDOUT)
-    print('Done bringing up interface {}'.format(INTERFACE_NAME))
-except subprocess.CalledProcessError, err:
-    print('Failed to bring up interface {}'.format(INTERFACE_NAME))
-    print('Received return code: {}'.format(err.returncode))
-    print('Received error message: {}'.format(err.output))
+    print('Bringing up interface {}'.format(iface_name))
+    try:
+        subprocess.check_output(
+            ['ifconfig', iface_name, 'up'],
+            stderr = subprocess.STDOUT
+        )
+        print('Done bringing up interface {}'.format(iface_name))
+        return True
+    except subprocess.CalledProcessError as err:
+        print('Failed to bring up interface {}'.format(iface_name))
+        print('Received return code: {}'.format(err.returncode))
+        print('Received error message: {}'.format(err.output))
+        return False
+
+def iface_down(iface_name):
+    """Shut down a network interface."""
+
+    print('Bringing down interface {}'.format(iface_name))
+    try:
+        subprocess.check_output(
+            ['ifconfig', iface_name, 'down'],
+            stderr = subprocess.STDOUT
+        )
+        print('Done bringing down interface {}'.format(iface_name))
+        return True
+    except subprocess.CalledProcessError as err:
+        print('Failed to bring down interface {}'.format(iface_name))
+        print('Received return code: {}'.format(err.returncode))
+        print('Received error message: {}'.format(err.output))
+        return False
+
+def main():
+    """Test the functions defined elsewhere in this file."""
+    iface_name = 'eth0'
+
+    iface_down(iface_name)
+    iface_up(iface_name)
+
+if '__main__' == __name__:
+    main()
