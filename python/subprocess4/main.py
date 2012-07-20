68804dcbebd8d22a5b8ad8785d15b1c09b6bd2d3
Jeremy <ichimonji10@gmail.com>
2012-07-20 11:58:30 -0400

    Added python code for better serial device detection.

--- python/subprocess4/main.py
+++ python/subprocess4/main.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 '''Lists the device names of all serial ports on your system.'''
 
-# TODO: Have prefixes for device names, cannot yet figure out what devices are
-# physically present.
-
 import subprocess
 import re
 
 def get_serial_naming_scheme():
-    '''Figure out what names are used for serial devices on this system.
+    '''Figure out what name prefixes are used for serial devices on this system.
 
-    Serial block devices are typically named /dev/ttyS*. However, that's not a
-    strict rule. Devices may be named something else. This function returns a
-    tuple of prefixes used to name serial devices on this system. For example::
+    Serial devices are typically named /dev/ttyS*. However, that's not a strict
+    rule. Devices may be named something else. This function returns a tuple of
+    prefixes used to name serial devices on this system. For example::
 
         ('/dev/ttyS', '/dev/cua')
     
@@ -21,6 +18,7 @@ def get_serial_naming_scheme():
     device_name = re.compile('^serial *(\S+)')
     device_names = ()
 
+    # Read /proc/tty/drivers
     try:
         tty_devices = subprocess.check_output(
             ['cat', '/proc/tty/drivers'],
@@ -32,7 +30,9 @@ def get_serial_naming_scheme():
         print('Received error message: {}'.format(err.output))
         return device_names
 
-    # Have a list of all tty devices and their names. Get serial tty devices.
+    # example of tty_devices contents:
+    # serial               /dev/ttyS       4 64-111 serial
+    # pty_slave            /dev/pts      136 0-1048575 pty:slave
     for line in tty_devices.splitlines():
         match = device_name.search(line)
         if(match):
@@ -40,9 +40,81 @@ def get_serial_naming_scheme():
 
     return device_names
 
+def get_serial_devices():
+    '''Figure out the names of all real serial devices on this system.
+
+    Returns a dict of ``{device_path: settings_dict}`` key:value pairs where
+    settings_dict has the following keys:
+        
+        #. Path
+        #. UART
+        #. Port
+        #. IRQ
+
+    As recently as Ubuntu 11.04, the devices listed in /proc/tty/driver/serial
+    did not correspond to real, physical devices. The output of "setserial -g
+    /dev/*" was similarly fictitious. Instead, the output showed whatever
+    logical configuration the kernel had for things like IRQ addresses and line
+    speed.
+
+    Ubuntu 12.04 introduces the use of the serinfo driver. This driver
+    automagically determines how many "real" serial devices are present. Here's
+    an example from a system with one serial port::
+
+        serinfo:1.0 driver revision:
+        0: uart:16550A port:000003F8 irq:4 tx:0 rx:0
+        1: uart:unknown port:000002F8 irq:3
+        2: uart:unknown port:000003E8 irq:4
+        3: uart:unknown port:000002E8 irq:3
+
+    Serial devices are conventionally prefixed /dev/ttyS. Serial devices may
+    also exist at /dev/ttyUSB, but logistix is not currently expected to test
+    usb-to-serial adapters. The number in the leftmost column above is appended
+    to the prefix, so the serial device in the example above is located at
+    /dev/ttyS0.
+
+    '''
+    proc_devices = ''
+    real_devices = {}
+
+    # Read /proc/tty/driver/serial
+    try:
+        proc_devices = subprocess.check_output(
+            ['cat', '/proc/tty/driver/serial'],
+            stderr = subprocess.STDOUT
+        )
+    except subprocess.CalledProcessError as err:
+        print('Failed to get info about serial devices.')
+        print('Received return code: {}'.format(err.returncode))
+        print('Received error message: {}'.format(err.output))
+        return real_devices
+
+    for line in proc_devices.splitlines():
+        uart_type = re.compile('uart:(\S+)').search(line)
+        if(uart_type and uart_type.group(1) != 'unknown'):
+            # Found a real serial device!
+            device_num  = re.compile('^(\d+)').search(line).group(1)
+            device_path = '/dev/ttyS' + device_num
+            port_num    = re.compile('port:(\S+)').search(line).group(1)
+            irq_num     = re.compile('irq:(\S+)').search(line).group(1)
+            real_devices[device_path] = {
+                'Path': device_path,
+                'UART': uart_type,
+                'Port': port_num,
+                'IRQ':  irq_num,
+            }
+
+    return real_devices
+
 def main():
     '''The intended entry point for this program.'''
-    print(get_serial_naming_scheme())
+
+    print('Serial devices have the following prefixes:\n{}'.format(
+        get_serial_naming_scheme()
+    ))
+    print('The following serial device info was found:\n{}'.format(
+        get_serial_devices()
+    ))
 
 if '__main__' == __name__:
     main()
