84930a025dd3c9aadf421bf44f10624d490d83c3
ichimonji10 <ichimonji10@gmail.com>
2012-08-23 13:28:20 -0400

    Added hello, world java program.

---
+++ java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * A very simple hello, world program in java.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
