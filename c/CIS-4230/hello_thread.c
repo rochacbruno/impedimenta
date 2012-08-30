e630bc48f43db112bac91300dab626a4b23bd902
ichimonji10 <ichimonji10@gmail.com>
2012-08-29 20:19:30 -0400

    Renamed hello_world.c -> hello_thread.c, and cleaned it up.

--- c/CIS-4230/hello_thread.c
+++ c/CIS-4230/hello_thread.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #define NUM_THREADS 10
 
@@ -8,27 +9,24 @@
  */
 void * say_hello(void * arg) {
     int hello_id = *(int *)arg; // dereference pointer to int
-
     printf("Hello, world! (thread arg: %d)\n", hello_id);
-
     return NULL;
 }
 
 /* Creates NUM_THREADS threads, joins them, and prints their return vals. */
 int main(void) {
-    pthread_t thread_ID[NUM_THREADS];
-    int args[NUM_THREADS];
-    void * ret_vals[NUM_THREADS];
+    pthread_t   thread_ID[NUM_THREADS];
+    int         args[NUM_THREADS];
+    void *      ret_vals[NUM_THREADS];
 
     for(int i = 0; i < NUM_THREADS; i++) {
         args[i] = i;
         pthread_create(&thread_ID[i], NULL, say_hello, &args[i]);
     }
     for(int i = 0; i < NUM_THREADS; i++) {
+        printf("joining thread %d\n", i);
         pthread_join(thread_ID[i], &ret_vals[i]);
-    }
-    for(int i = 0; i < NUM_THREADS; i++) {
-        printf("Return value for thread %d: %d\n", i, ret_vals[i]);
+        printf("thread %d returned: %d\n", i, ret_vals[i]);
     }
 
     return 0;
