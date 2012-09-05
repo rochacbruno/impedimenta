603eb9ae90c5abe807ab21136c2c289d97e57e9f
ichimonji10 <ichimonji10@gmail.com>
2012-09-04 20:30:11 -0400

    Updated class Main to provide a better demonstration of class Node.

--- java/generics1/Main.java
+++ java/generics1/Main.java
@@ -1,13 +1,26 @@
 package generics1;
 import generics1.Node;
 
+/**
+ * The Main class exercises other classes in package generics1.
+ */
 public class Main {
+    /**
+     * Class Node provides the foundation for a linked list. A LinkedList class
+     * has not yet been written, but this method demonstrates, in a nutshell,
+     * how to "walk" down a linked list.
+     */
     public static void main(String[] args) {
         Node<String> myStringNode = new Node<String>("I'm the tail node.");
         Node<Integer> myIntNode = new Node<Integer>(42, myStringNode);
         Node<Object> head = new Node<Object>(null, myIntNode);
 
-        System.out.println(head.next.data.toString());
-        System.out.println(head.next.next.data.toString());
+        System.out.println(
+            "Head cannot be printed, because it's \"data\" member is null. "
+            + "But the next two nodes in this linked list can.\n"
+            + "head.data: null\n"
+            + "head.next.data: " + head.next.data.toString() + "\n"
+            + "head.next.next.data: " + head.next.next.data.toString()
+        );
     }
 }
