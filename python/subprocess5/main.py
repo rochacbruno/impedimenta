2df7b7971e71a0edcfd0b08f15b8c8c49106868b
Jeremy <ichimonji10@gmail.com>
2012-09-07 11:35:44 -0400

    Expanded python subprocess.Popen example

--- python/subprocess5/main.py
+++ python/subprocess5/main.py
@@ -7,7 +7,8 @@ import time
 SUBPROCESS_MAX_RUNTIME = 5
 
 def main():
-    memtester_thread = subprocess.Popen(
+    print('printing output as it arrives')
+    ping_thread = subprocess.Popen(
         [
             'timeout',
             str(SUBPROCESS_MAX_RUNTIME), # seconds
@@ -17,15 +18,31 @@ def main():
         stdout = subprocess.PIPE,
         stderr = subprocess.PIPE,
     )
-    while(None == memtester_thread.poll()):
+    while(None == ping_thread.poll()):
         print('stdout: {}'.format(
-            memtester_thread.stdout.readline().rstrip()
+            ping_thread.stdout.readline().rstrip()
         ))
         # oh no!
 #        print('stderr: {}'.format(
-#            memtester_thread.stderr.readline().rstrip()
+#            ping_thread.stderr.readline().rstrip()
 #        ))
         time.sleep(1)
+
+    print('\ncaching all output before printing')
+    ping_thread = subprocess.Popen(
+        [
+            'timeout',
+            str(SUBPROCESS_MAX_RUNTIME), # seconds
+            'ping',
+            'google.com',
+        ],
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+    )
+    while(None == ping_thread.poll()):
+        print('waiting...')
+        time.sleep(1)
+    print('output: {}'.format(ping_thread.communicate()))
 
 if '__main__' == __name__:
     sys.exit(main())
