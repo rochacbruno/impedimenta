1dc7d509d2b96518c8bdf75ca8494de5c1177024
ichimonji10 <ichimonji10@gmail.com>
2012-09-16 20:49:07 -0400

    Added function prototypes to thread_pool.{c,h}

--- c/CIS-4230/hw2/thread_pool.c
+++ c/CIS-4230/hw2/thread_pool.c
@@ -1,1 +1,13 @@
+#include "thread_pool.h"
 
+void thread_pool_init(struct thread_pool * pool) {
+    return;
+}
+
+void thread_pool_work(struct work_unit * work) {
+    return;
+}
+
+void thread_pool_die() {
+    return;
+}
