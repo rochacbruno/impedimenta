958d4491fd5ce31764189f532a33c2f1dc132628
ichimonji10 <ichimonji10@gmail.com>
2012-10-03 00:13:26 -0400

    Started working on a parallel version of the gaussian solver.

---
+++ c/CIS-4230/prog1/Parallel/linear_equations.h
@@ -0,0 +1,19 @@
+/*!
+    \file   linear_equations.h
+    \brief  Interface to linear equation solver.
+    \author (C) Copyright 2012 by Peter C. Chapin <PChapin@vtc.vsc.edu>
+*/
+
+#ifndef LINEAR_EQUATIONS_H
+#define LINEAR_EQUATIONS_H
+
+typedef double floating_type;
+
+void gaussian_solve(
+    int size,
+    floating_type a[size][size],
+    floating_type b[size],
+    int * error
+);
+
+#endif
---
+++ c/CIS-4230/prog1/Parallel/solve_system.c
@@ -0,0 +1,62 @@
+/*!
+    \file   solve_system.c
+    \brief  Solve a large system of simultaneous equations.
+    \author (C) Copyright 2012 by Peter C. Chapin <PChapin@vtc.vsc.edu>
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <Timer.h>
+#include "linear_equations.h"
+
+int main(int argc, char * argv[]) {
+    FILE * input_file;
+    int size;
+
+    // Check for arg and validity of that arg.
+    if(argc != 2) {
+        printf("Error: Expected the name of a system definition file.\n");
+        return EXIT_FAILURE;
+    }
+    if((input_file = fopen(argv[1], "r")) == NULL) {
+        printf("Error: Can not open the system definition file.\n");
+        return EXIT_FAILURE;
+    }
+
+    // Get the size of the gaussian equation, then create arrays to hold it.
+    // WARNING: this should be malloc'd instead. Creating a[size][size] results
+    // in segfaults for large ``size``. (on my machine, 1100+ will do it)
+    fscanf(input_file, "%d", &size);
+    floating_type a[size][size];
+    floating_type b[size];
+    // Populate arrays with coefficients.
+    for(int i = 0; i < size; ++i) {
+        for(int j = 0; j < size; ++j) {
+            fscanf(input_file, "%lf", &a[i][j]); // Nasty type unsafety!
+        }
+        fscanf(input_file, "%lf", &b[i]); // Here too!
+    }
+    fclose(input_file);
+
+    // Solve the equation.
+    Timer stopwatch;
+    Timer_start(&stopwatch);
+    int error;
+    gaussian_solve(size, a, b, &error);
+    Timer_stop(&stopwatch);
+    free(a);
+
+    // Print the solution.
+    if(error) {
+        printf("System is degenerate\n");
+    } else {
+        printf("\nSolution is\n");
+        for(int i = 0; i < size; ++i) {
+            printf(" x(%4d) = %9.5f\n", i, b[i]);
+        }
+        printf("\nExecution time = %ld milliseconds\n", Timer_time(&stopwatch));
+    }
+    free(b);
+
+    return EXIT_SUCCESS;
+}
