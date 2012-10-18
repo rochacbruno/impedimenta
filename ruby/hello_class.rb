b065e655197d5cd3aedd5ff30ebe02d8bc014786
ichimonji10 <ichimonji10@gmail.com>
2012-10-18 11:31:41 -0400

    Updated .bashrc and removed vim colorization hack. Updated hello_class.rb

--- ruby/hello_class.rb
+++ ruby/hello_class.rb
@@ -3,64 +3,66 @@
 # About
 # =====
 #
-# Demonstrates how to declare and instantiate classes, and how to modify objects
-# at runtime. This script focuses more on showing off features than doing
-# anything useful.
+# Demonstrates some basic facts about Ruby classes. This script focuses more on
+# showing off features than doing anything useful. This is largely ripped from
+# "The Ruby Programming Language", by David Flanagan.
 #
 
-class VerbosePoint
-    # constructor
+# By default, methods are public. You can change them explicitly like shown
+# below, or you can place the keywords in the class definition a la C++. ::
+#
+#    public     :initialize, :to_s
+#    protected
+#    private    :x, :y
+
+class Point
+    @@num_points = 0
+
     def initialize(x, y)
         @x, @y = x, y
+        @@num_points += 1
     end
 
-    # accessor, but not mutator
-    def x
-        @x
-    end
-
-    # accessor, but not mutator
-    def y
-        @y
-    end
-
-    # to string
-    def to_s
-        "(#{@x},#{@y})"
-    end
-
-    # class method
-    def VerbosePoint.classMethod
-    end
-
-# by default, methods are public
-#protected   # following are protected
-#private     # following are private
-#public      # following are public
-end
-
-class CompactPoint
-    def initialize(x, y)
-        @x, @y = x, y
-    end
-
+    # cannot occur before initialize
     attr_reader(:x, :y)
+    # alternately:
+    # def x; @x; end
+    # def y; @y; end
+
+    ORIGIN = self.new(0, 0)
 
     def to_s
-        "(#@x,#@y)"
+        "(#@x,#@y)" # note: no curly braces for single var substitution
     end
 
-    def self.classMethod
+    # vector addition
+    def +(other)
+        # Must explicitly prefix new with ``Point.``, because not class method.
+        Point.new(@x + other.x, @y + other.y)
+    end
+
+    # Class method. Alternately: def Point.num_points; @@num_points; end
+    def self.num_points
+        @@num_points
+    end
+
+    # add an arbitrary number of ``Point``s
+    def self.sum(*points)
+        x = y = 0
+        points.each { |point|
+            x += point.x
+            y += point.y
+        }
+        self.new(x, y)
     end
 end
 
 # OK, done making definitions. Do something with them!
-point1 = VerbosePoint.new(1, 2)
-point2 = CompactPoint.new(2, 3)
-# can this be looped?
-puts "point1"
-puts "    ID: #{point1.object_id}"
-puts "    value: #{point1.to_s}"
-puts "point2"
-puts "    ID: #{point2.object_id}"
-puts "    value: #{point2}"
+puts "point: #{point = Point.new(1, 2)}"
+puts "object_id: #{point.object_id}"
+puts "to_s: #{point}"
+puts "#{point} + #{point}: #{point + point}"
+puts "Point.sum(%s, %s, %s): %s" %
+      [point, point, point, Point.sum(point, point, point)]
+puts "Point.num_points: #{Point.num_points}"
+puts "Point::ORIGIN: #{Point::ORIGIN}"
