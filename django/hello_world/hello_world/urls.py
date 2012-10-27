0128730224ad424bc244aff3469667f5a39e5771
ichimonji10 <ichimonji10@gmail.com>
2012-10-27 10:46:37 -0400

    Added another example to the hello_world django app.

--- django/hello_world/hello_world/urls.py
+++ django/hello_world/hello_world/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^$', views.index),
     url(r'^hello/$', views.hello),
     url(r'^time/$', views.time),
+    url(r'^time/plus/(-?\d{1,3})/$', views.time_plus),
     # Examples:
     # url(r'^$', 'hello_world.views.home', name='home'),
     # url(r'^hello_world/', include('hello_world.foo.urls')),
--- django/hello_world/hello_world/views.py
+++ django/hello_world/hello_world/views.py
@@ -12,3 +12,14 @@ def time(request):
         datetime.datetime.now()
     )
     return http.HttpResponse(html)
+
+def time_plus(request, hour_offset):
+    try:
+        hour_offset = int(hour_offset)
+    except ValueError:
+        raise http.Http404()
+    time = datetime.datetime.now()
+    html = "<html><body>In {} hours, it will be {}.</html></body>".format(
+        hour_offset, time + datetime.timedelta(hours = hour_offset)
+    )
+    return http.HttpResponse(html)
