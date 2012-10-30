16f56039348d36d907bbe91c96e0e85cf9b12cd4
Jeremy Audet <ichimonji10@gmail.com>
2012-10-30 00:37:14 -0500

    Updated update_www_1.sh script so that it updates bzr dirs too.

--- bash/update_www_1.sh
+++ bash/update_www_1.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-# Update the www_1 folder, convert markdown files therein to HTML, and link www
-# to www_1.
+# Update the $HTML_DEST folder, convert markdown files therein to HTML, and link
+# www to $HTML_DEST.
+
+HTML_DEST="www_1"
 
 cd /media/external1/lighttpd
-bzr clean-tree --detritus --force --directory www_1
-bzr update www_1
-find "www_1" -type f -name \*.markdown | while read filename; do
+for dir in $HTML_DEST templates; do
+    bzr clean-tree --detritus --force --directory $dir
+    bzr update $dir
+done
+find "$HTML_DEST" -type f -name \*.markdown | while read filename; do
     ./populate_template.py ./templates/default.html "$filename" > "${filename%markdown}html"
 done
-ln -snf ./www_1 ./www
+ln -snf "$HTML_DEST" www
