c694a964668dbb29a6c09d3633881702f194d7b1
Jeremy Audet <ichimonji10@gmail.com>
2012-11-11 01:30:06 -0500

    More updates to scala hw4 parser.

--- scala/hw4/Parser.scala
+++ scala/hw4/Parser.scala
@@ -1,17 +1,34 @@
 import scala.util.parsing.combinator._
 
-class Arith extends JavaTokenParsers {
-    def expr:   Parser[Any] = term~rep("+"~term | "-"~term)
-    def term:   Parser[Any] = factor~rep("*"~factor | "/"~factor)
-    def factor: Parser[Any] = floatingPointNumber | "("~expr~")"
-}
+/**
+ * A parser for simple algebraic expressions. Can deal with floating point
+ * numbers, addition, subtraction, multiplication, division, and parentheses.
+ */
+class Custom extends JavaTokenParsers {
+    /**
+     * Parses expressions like 2.5~+~1.5~-~4.0. (would return 0.0)
+     */
+    def expr: Parser[Double] = term~rep("+"~term | "-"~term) ^^ {
+        case a~b => {
+            var num1 = a
+            for(operatorAndNum <- b) {
+                operatorAndNum match {
+                    case "+"~num2 => num1 += num2
+                    case "-"~num2 => num1 -= num2
+                }
+            }
+            num1
+        }
+    }
 
-class Custom extends RegexParsers {
+    /**
+     * Parses expressions like 2.0~*~3.0~/~1.5. (would return 4.0)
+     */
     def term: Parser[Double] = number~rep("*"~number | "/"~number) ^^ {
         case a~b => {
             var num1 = a
-            for(operatorNum <- b) {
-                operatorNum match {
+            for(operatorAndNum <- b) {
+                operatorAndNum match {
                     case "*"~num2 => num1 *= num2
                     case "/"~num2 => num1 /= num2
                 }
@@ -19,12 +36,17 @@ class Custom extends RegexParsers {
             num1
         }
     }
-    def number: Parser[Double] = """\d+(\.\d+)?""".r ^^ (x => x.toDouble)
+
+    // def factor: Parser[Any] = floatingPointNumber | "("~expr~")"
+    def number: Parser[Double] = floatingPointNumber ^^ (x => x.toDouble)
 }
 
+/**
+ * Calls the parser defined above and passes it <code>args(0)</code>.
+ */
 object Parser extends Custom {
     def main(args: Array[String]) {
         println("input : "+ args(0))
-        println(parseAll(term, args(0)))
+        println(parseAll(expr, args(0)))
     }
 }
--- scala/run.sh
+++ scala/run.sh
@@ -2,5 +2,5 @@
 
 scalac -cp scalatest_2.9.0-1.8.jar hw4/*.scala
 #scala -cp scalatest_2.9.0-1.8.jar org.scalatest.run TestTree
-scala Parser "2.0 * 3.0 / 4.0"
+scala Parser "20.0 + 2.0 * 3.0 / 1.5 - 10.0 * 2"
 scala Parser "2.0 / 3.0 * 4.0"
