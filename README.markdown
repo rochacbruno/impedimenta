7b00c444aa45a1e3de8c51433dcd1126a52804d9
Jeremy Audet <ichimonji10@gmail.com>
2012-11-25 23:57:42 -0500

    Fleshed out most of the MHS models. Fixed a typo in the impedimenta README.

--- README.markdown
+++ README.markdown
@@ -2,8 +2,8 @@ About
 =====
 
 This repo is a warehouse for code I've written. Much of it has been written for
-school assignment, but there's also personal projects and non-critical code from
-work. Most of this code is utter crap, and there's no guarantee about when I'll
-decide to do some spring cleaning.
+school assignments, but there's also personal projects and non-critical code
+from work. Most of this code is utter crap, and there's no guarantee about when
+I'll decide to do some spring cleaning.
 
 There is no particular focus to this repo, nor should there be.
--- django/generic_project/mhs/models.py
+++ django/generic_project/mhs/models.py
@@ -1,39 +1,60 @@
-#from django.db import models
+from django.db import models
 
-#class Author(models.Model):
-#    first_name = models.CharField(max_length=30)
-#    last_name = models.CharField(max_length=40)
-#    email = models.EmailField()
+# TODO: Flesh out Visit. Allow Patient to reference Visit.
 
-#patient
-#    name
-#    phone numbers
-#    address
-#    date of birth
-#    place of birth
-#    social security number
-#    health problems
-#    emergency contact, primary care doctor, insurance providres, history of visits...
-#emergency contact
-#    name
-#    phone numbers
-#primary care doctor:
-#    name
-#    phone numbers
-#    address
-#insurance providers
-#    name
-#    phone numbers
-#    address
-#history of visits
-#    date
-#    location
-#    notes about visit
-#    reason for visit
-#        emergency
-#        see specialist
-#        surgery
-#        outpatient procedure
-#        rehab
-#        follow up visit
-#        other
+class Patient(models.Model):
+    '''A patient of Modern Hospital Systems.'''
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    address = models.CharField(max_length = 100)
+    birth_date = models.DateField()
+    birth_place = models.CharField(max_length = 100)
+    social_security = models.CharField(max_length = 9)
+    health_issues = models.TextField()
+    number = models.CharField(max_length = 25)
+    # emergency contact, primary care doctor, insurance providres, history of visits...
+
+    def __unicode__(self):
+        return '{} {}'.format(first_name, last_name)
+
+class EmergencyContact(models.Model):
+    '''An emergency contact for a ``Patient``.'''
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    number = models.CharField(max_length = 25)
+
+    def __unicode__(self):
+        return '{} {}'.format(first_name, last_name)
+
+class Doctor(models.Model):
+    '''A primary care doctor for a ``Patient``.'''
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    address = models.CharField(max_length = 100)
+    number = models.CharField(max_length = 25)
+
+    def __unicode__(self):
+        return '{} {}'.format(first_name, last_name)
+
+class InsuranceProvider(models.Model):
+    '''An insurance provider for a ``Patient``.'''
+    name = models.CharField(max_length = 100)
+    address = models.CharField(max_length = 100)
+    number = models.CharField(max_length = 25)
+
+    def __unicode__(self):
+        return self.name
+
+class Visit(models.Model):
+    '''Represents a single visit by a ``Patient`` to an MHS location.'''
+    date = models.DateTimeField()
+    location = models.CharField(max_length = 50)
+    notes = models.TextField()
+    # reason for visit
+    #     emergency
+    #     see specialist
+    #     surgery
+    #     outpatient procedure
+    #     rehab
+    #     follow up visit
+    #     other
