7a3dcf334e01494dd7f205d7055f4509735d1ae9
Jeremy Audet <ichimonji10@gmail.com>
2013-04-12 17:27:16 -0400

    Add vanilla "comments" Rails 3 application.
    Whereas the current "poll" Rails application runs on Rails 2, "comments" runs
    on Rails 3. There are numerous other differences, such as using mysql instead
    of postgresql, and using Ruby 2.0.0-p0 instead of 1.9.3-p0.

---
+++ web/rails/comments/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comments::Application.config.secret_token = 'a9962dcb513545200be672ff341a42c3a67e8dffd453af19952a818e9a3e350f6747da1e141df092e97e68338263e5bcfe8c538927b7a7ec5290b9dcc0eb72c6'
