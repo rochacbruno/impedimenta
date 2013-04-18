df04b571079c3338928d445d5b23897a31e5311f
Jeremy Audet <ichimonji10@gmail.com>
2013-04-18 11:46:43 -0400

    Add navbar to website. Simplify foo#index template

--- web/rails/comments/app/controllers/foo_controller.rb
+++ web/rails/comments/app/controllers/foo_controller.rb
@@ -1,8 +1,12 @@
 class FooController < ApplicationController
     layout('base')
 
+    # A simple landing page which displays a static message to the user.
     def index
         @message = 'Hello!'
-        @count = 3
+    end
+
+    # Allows user to enter text into a form, which is then echoed back to them.
+    def echo
     end
 end
