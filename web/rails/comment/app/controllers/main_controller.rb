27a5bfe35497704ed5fda4116b39914a72f09e4d
Jeremy Audet <ichimonji10@gmail.com>
2013-04-19 15:10:57 -0400

    Rename methods in controller `main`.
    The methods (or actions) in main have been renamed, and one has been added. The
    result is as follows:
    
        main#create => main#create_comment
        main#read   => main#read_comment
                    => main#read_comment_id
    
    The routing file has been updated to reflect these changes.

--- web/rails/comment/app/controllers/main_controller.rb
+++ web/rails/comment/app/controllers/main_controller.rb
@@ -2,12 +2,22 @@ class MainController < ApplicationController
     layout('base')
 
     # Create a new comment.
-    def create
-        redirect_to(:read) #unless request.post?
+    def create_comment
+        redirect_to(:read_comment) #unless request.post?
     end
 
     # Return a list of all comments.
-    def read
+    def read_comment
         @comments = Comment_.limit(100)
+    end
+
+    # Return a specific comment.
+    def read_comment_id
+        begin
+            @comment = Comment_.find(params['id'])
+        rescue ActiveRecord::RecordNotFound => err
+            @comment = nil
+            @id = params['id']
+        end
     end
 end
