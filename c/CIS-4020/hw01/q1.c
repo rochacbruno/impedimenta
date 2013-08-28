7b7c94e01bb0d21e728bb208228f1339a48f87fd
Jeremy Audet <ichimonji10@gmail.com>
2013-08-28 17:22:59 -0400

    Add C program which makes several system calls

---
+++ c/CIS-4020/hw01/q1.c
@@ -0,0 +1,29 @@
+/* Make several system calls.
+
+According to syscalls(2), "System  calls  are  generally not invoked directly,
+but rather via wrapper functions in glibc (or perhaps some other library)."
+For example, glibc provides the wrapper function `getpid`. However, system calls
+can also be made directly. <sys/syscall.h> provides the `syscall` function for
+doing just this.
+
+Useful references:
+
+* /usr/include/asm/unistd.h
+* /usr/include/sys/syscall.h
+* intro(2)
+* syscall(2)
+* syscalls(2)
+
+*/
+#include <sys/syscall.h>
+#include <stdio.h>
+
+int main(void) {
+    printf("getpid(): %ld\n", getpid());
+    printf("syscall(__NR_getpid): %ld\n", syscall(__NR_getpid));
+    printf("syscall(SYS_getpid): %ld\n", syscall(SYS_getpid));
+    printf("syscall(39): %ld\n", syscall(39));
+    printf("syscall(5000): %ld\n", syscall(5000));
+
+    return(0);
+}
