f901d87f68ecba10ef02903c34cc9580a77b3cb6
Jeremy Audet <ichimonji10@gmail.com>
2014-03-09 11:54:22 -0400

    Update script for generating Garmin map files
    Refactor script. Properly quote variables, add some commenting, remove spurious
    quotes, change some double quotes to single quotes and replace `...` with
    $(...).

--- useful_things/scripts/osm-build.sh
+++ useful_things/scripts/osm-build.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env sh
+#
+# Generate a Garmin-compatible map file for a certain geographic region.
+#
 
-region='us-northeast-latest'
+region=us-northeast-latest
 
-wget http://download.geofabrik.de/north-america/${region}.osm.pbf
-splitter ${region}.osm.pbf
-osmconvert ${region}.osm.pbf --out-o5m > ${region}.o5m
-osmfilter ${region}.o5m \
+wget "http://download.geofabrik.de/north-america/${region}.osm.pbf"
+splitter "${region}.osm.pbf"
+osmconvert "${region}.osm.pbf" --out-o5m > "${region}.o5m"
+osmfilter "${region}.o5m" \
     --keep-nodes= \
-    --keep-ways-relations="boundary=administrative =postal_code postal_code=" \
+    --keep-ways-relations='boundary=administrative =postal_code postal_code=' \
     --out-o5m \
-    > ${region}-boundaries.o5m
+    > "${region}-boundaries.o5m"
 java -cp /usr/share/java/mkgmap/mkgmap.jar \
     uk.me.parabola.mkgmap.reader.osm.boundary.BoundaryPreprocessor \
-    ${region}-boundaries.o5m \
+    "${region}-boundaries.o5m" \
     bounds
 mkgmap \
     --route \
@@ -20,5 +23,5 @@ mkgmap \
     --bounds=bounds \
     --index \
     --gmapsupp \
-    --mapname=`echo $RANDOM | sed -e :a -e 's/^.\{1,7\}$/&0/;ta'` \
+    --mapname=$(echo "${RANDOM}" | sed -e :a -e 's/^.\{1,7\}$/&0/;ta') \
     6324*.osm.pbf
