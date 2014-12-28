147e024a3c6fc59a71cfcb230a4e6232ec53bfe6
Jeremy Audet <ichimonji10@gmail.com>
2014-12-28 01:35:32 -0500

    Document "hello.c" with doxygen. Update makefile.
    Use doxygen to document a C hello world program. Update the makefile for this
    program:
    
    * Add targets for generating documentation (`docs`), removing temporary files
      (`clean`) and printing a help message (`help`).
    * Change the the default target from `hello` to `help`.

--- c/21CC/01-hello-world/hello.c
+++ c/21CC/01-hello-world/hello.c
@@ -1,5 +1,13 @@
+/**
+ * @file
+ * @brief An extremely simple hello world program.
+ */
 #include <stdio.h>
 
+/**
+ * @brief Print a hello world message.
+ * @return 0
+ */
 int main() {
     printf("Hello, world.\n");
     return 0;
