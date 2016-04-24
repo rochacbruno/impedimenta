f616db895c9bc2c65c89ebe351067771c56170cd
Jeremy Audet <ichimonji10@gmail.com>
2016-04-24 16:27:19 -0400

    Add a template Borg Backup script

---
+++ useful_things/scripts/borg.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Create and prune Borg backups. See: http://borgbackup.readthedocs.org. If the
+# repository is on a remote system, ensure the SSH key is usable.
+#
+export BORG_PASSPHRASE=  # passphrase for repository
+repository=  # e.g. foo.example.com:borgbackup-myname
+archive_prefix="$(hostname --long)"
+archive_postfix="$(date --iso-8601=seconds)"
+
+borg create \
+    --compression lzma \
+    "${repository}::${archive_prefix}-${archive_postfix}" \
+    ~/Documents  # FIXME: customize this
+borg prune "${repository}" --prefix "${archive_prefix}-" \
+    --keep-within 8d --keep-weekly 4 --keep-monthly 6
