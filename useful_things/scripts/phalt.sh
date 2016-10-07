9b5e3afa0854107283efe11f3a123798bebebe53
Jeremy Audet <ichimonji10@gmail.com>
2016-10-07 14:41:30 -0400

    Delete several unused scripts

--- useful_things/scripts/phalt.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-#
-# Halt a Vagrant-managed Pulp VM that has been rolled back from 2.8 to 2.7.
-
-touch playpen/bootstrap-ansible.sh
-install -D /dev/null playpen/ansible/vagrant-playbook.yml
-vagrant halt
