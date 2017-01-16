4c5241f5aeda258f6fa426334bd6350a27de6f80
Jeremy Audet <ichimonji10@gmail.com>
2017-01-16 14:15:15 -0500

    Add permutation to psmash-runner script
    Include fedora-25-pulp-2-11

--- useful_things/scripts/tmux/psmash-runner.sh
+++ useful_things/scripts/tmux/psmash-runner.sh
@@ -4,7 +4,11 @@
 # at once.
 set -euo pipefail
 
-# create six panes
+# create eight panes
+tmux split-window -v
+tmux select-layout even-vertical
+tmux split-window -v
+tmux select-layout even-vertical
 tmux split-window -v
 tmux select-layout even-vertical
 tmux split-window -v
@@ -19,10 +23,12 @@ tmux select-layout even-vertical
 # configure each pane
 tmux send-keys -t 0 'export PULP_SMASH_CONFIG_FILE=fedora-24-pulp-2-11.json' C-m
 tmux send-keys -t 1 'export PULP_SMASH_CONFIG_FILE=fedora-24-pulp-2-12.json' C-m
-tmux send-keys -t 2 'export PULP_SMASH_CONFIG_FILE=rhel-6-8-pulp-2-11.json' C-m
-tmux send-keys -t 3 'export PULP_SMASH_CONFIG_FILE=rhel-6-8-pulp-2-12.json' C-m
-tmux send-keys -t 4 'export PULP_SMASH_CONFIG_FILE=rhel-7-3-pulp-2-11.json' C-m
-tmux send-keys -t 5 'export PULP_SMASH_CONFIG_FILE=rhel-7-3-pulp-2-12.json' C-m
+tmux send-keys -t 2 'export PULP_SMASH_CONFIG_FILE=fedora-25-pulp-2-11.json' C-m
+tmux send-keys -t 3 'export PULP_SMASH_CONFIG_FILE=fedora-25-pulp-2-12.json' C-m
+tmux send-keys -t 4 'export PULP_SMASH_CONFIG_FILE=rhel-6-8-pulp-2-11.json' C-m
+tmux send-keys -t 5 'export PULP_SMASH_CONFIG_FILE=rhel-6-8-pulp-2-12.json' C-m
+tmux send-keys -t 6 'export PULP_SMASH_CONFIG_FILE=rhel-7-3-pulp-2-11.json' C-m
+tmux send-keys -t 7 'export PULP_SMASH_CONFIG_FILE=rhel-7-3-pulp-2-12.json' C-m
 
 # etc
 tmux set-window-option synchronize-panes on
