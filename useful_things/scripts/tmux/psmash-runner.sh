971b020720ca2d7d485e492c1f13941cf3fc9a07
Jeremy Audet <ichimonji10@gmail.com>
2017-04-01 00:03:59 +0000

    Update psmash-runner.sh tmux script
    Add RHEL 7 Pulp 2.13 to spawned panes.

--- useful_things/scripts/tmux/psmash-runner.sh
+++ useful_things/scripts/tmux/psmash-runner.sh
@@ -13,6 +13,8 @@ tmux split-window -v
 tmux select-layout even-vertical
 tmux split-window -v
 tmux select-layout even-vertical
+tmux split-window -v
+tmux select-layout even-vertical
 
 # configure each pane
 tmux send-keys -t 0 'export PULP_SMASH_CONFIG_FILE=fedora-24-pulp-2-12.json' C-m
@@ -21,7 +23,7 @@ tmux send-keys -t 2 'export PULP_SMASH_CONFIG_FILE=fedora-25-pulp-2-12.json' C-m
 tmux send-keys -t 3 'export PULP_SMASH_CONFIG_FILE=fedora-25-pulp-2-13.json' C-m
 # tmux send-keys -t 4 'export PULP_SMASH_CONFIG_FILE=rhel-6-8-pulp-2-13.json' C-m
 tmux send-keys -t 4 'export PULP_SMASH_CONFIG_FILE=rhel-7-3-pulp-2-12.json' C-m
-# tmux send-keys -t 6 'export PULP_SMASH_CONFIG_FILE=rhel-7-3-pulp-2-13.json' C-m
+tmux send-keys -t 5 'export PULP_SMASH_CONFIG_FILE=rhel-7-3-pulp-2-13.json' C-m
 
 # etc
 tmux set-window-option synchronize-panes on
