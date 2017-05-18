210cb7ee59ada04106b717415c1b67ad98c468c0
Jeremy Audet <ichimonji10@gmail.com>
2017-05-18 22:55:42 +0000

    Update psmash-runner.sh script
    Pulp 2.13 is out, and 2.14 is under development. Let the script spawn
    tmux panes for these versions of Pulp.

--- useful_things/scripts/tmux/psmash-runner.sh
+++ useful_things/scripts/tmux/psmash-runner.sh
@@ -17,13 +17,12 @@ tmux split-window -v
 tmux select-layout even-vertical
 
 # configure each pane
-tmux send-keys -t 0 'export PULP_SMASH_CONFIG_FILE=fedora-24-pulp-2-12.json' C-m
-tmux send-keys -t 1 'export PULP_SMASH_CONFIG_FILE=fedora-24-pulp-2-13.json' C-m
-tmux send-keys -t 2 'export PULP_SMASH_CONFIG_FILE=fedora-25-pulp-2-12.json' C-m
-tmux send-keys -t 3 'export PULP_SMASH_CONFIG_FILE=fedora-25-pulp-2-13.json' C-m
-# tmux send-keys -t 4 'export PULP_SMASH_CONFIG_FILE=rhel-6-8-pulp-2-13.json' C-m
-tmux send-keys -t 4 'export PULP_SMASH_CONFIG_FILE=rhel-7-3-pulp-2-12.json' C-m
-tmux send-keys -t 5 'export PULP_SMASH_CONFIG_FILE=rhel-7-3-pulp-2-13.json' C-m
+tmux send-keys -t 0 'export PULP_SMASH_CONFIG_FILE=fedora-24-pulp-2-13.json' C-m
+tmux send-keys -t 1 'export PULP_SMASH_CONFIG_FILE=fedora-24-pulp-2-14.json' C-m
+tmux send-keys -t 2 'export PULP_SMASH_CONFIG_FILE=fedora-25-pulp-2-13.json' C-m
+tmux send-keys -t 3 'export PULP_SMASH_CONFIG_FILE=fedora-25-pulp-2-14.json' C-m
+tmux send-keys -t 4 'export PULP_SMASH_CONFIG_FILE=rhel-7-3-pulp-2-13.json' C-m
+tmux send-keys -t 5 'export PULP_SMASH_CONFIG_FILE=rhel-7-3-pulp-2-14.json' C-m
 
 # etc
 tmux set-window-option synchronize-panes on
