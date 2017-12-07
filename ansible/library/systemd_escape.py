10835a00479f4d0b0d752f8b2a07c7999051c148
Jeremy Audet <ichimonji10@gmail.com>
2017-12-07 11:48:11 -0500

    Shrink logical footprint of custom module

--- ansible/library/systemd_escape.py
+++ ansible/library/systemd_escape.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Given a path, return a systemd-escape(1) version."""
-import subprocess
-
 from ansible.module_utils.basic import AnsibleModule
 
 
@@ -12,10 +10,11 @@ def main():
         argument_spec={'path': {'required': True, 'type': 'str'}},
         supports_check_mode=True,
     )
-    escaped_path = subprocess.run(
-        ('systemd-escape', '--path', module.params['path']),
-        stdout=subprocess.PIPE,
-    ).stdout.strip()
+    escaped_path = module.run_command([
+        'systemd-escape',
+        '--path',
+        module.params['path'],
+    ])[1].strip()
     module.exit_json(changed=False, escaped_path=escaped_path)
 
 
