f5a477e251f0973c9552fc760d2e9619ce5fe71c
Jeremy Audet <ichimonji10@gmail.com>
2018-01-11 12:20:22 -0500

    Time individual steps in basic perf test
    The steps can then be graphed in a stacked bar chart.

--- bash/pulp/bin/step.sh
+++ bash/pulp/bin/step.sh
@@ -3,13 +3,6 @@
 
 repo_id="dst-$(uuidgen)"
 
-# Create, populate and publish a repository.
-steps() {
-    pulp-admin rpm repo create --repo-id "${repo_id}" >/dev/null
-    pulp-admin rpm repo copy all --from-repo-id src --to-repo-id "${repo_id}" >/dev/null
-    pulp-admin rpm repo publish run --repo-id "${repo_id}" >/dev/null
-}
-
 # Schedule teardown.
 teardown() {
     pulp-admin rpm repo delete --repo-id "${repo_id}" >/dev/null
@@ -21,5 +14,16 @@ trap 'teardown ; trap - SIGTERM ; kill -s SIGTERM $$' SIGTERM
 # Make (the bash builtin) time return values like '5.001' — that's five thousand
 # and one milliseconds — instead of prose.
 TIMEFORMAT='%E'
-# Time prints results to stderr.
-(time steps) 2>&1
+declare -a times
+
+# Create, populate and publish a repository.
+# shellcheck disable=SC2016
+cmds=(
+    'pulp-admin rpm repo create --repo-id "${repo_id}" >/dev/null'
+    'pulp-admin rpm repo copy all --from-repo-id src --to-repo-id "${repo_id}" >/dev/null'
+    'pulp-admin rpm repo publish run --repo-id "${repo_id}" >/dev/null'
+)
+for cmd in "${cmds[@]}"; do
+    times+=( "$(time ( eval "${cmd}" ) 2>&1)" )
+done
+echo "${times[*]}"
